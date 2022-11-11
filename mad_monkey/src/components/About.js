8c9a665cf2b4e36f8d1f1ad98f73c4cb3b153e64
Giriraj-Roy <girirajroy2001@gmail.com>
2022-11-12 00:32:13 +0530

    About us page done

--- mad_monkey/src/components/About.js
+++ mad_monkey/src/components/About.js
@@ -1,16 +1,36 @@
 import React from 'react'
-import img from '../assets/tie2.png'
+import img from '../assets/Tie.png'
 
 const About = () => {
   return (
-    <div className='flex'>
-      <div className='ml-10 p-32 bg-white rounded-xl w-1/3'>
-            <div className=''>
-              <img className='bg-transparent' src={img} alt='monkey'/>
-            </div>
+    <div className='block lg:flex mt-16 ml-7'>
+      <div className='w-4/5 lg:w-1/2 py-10 px-3 lg:px-0 lg:py-40 bg-[#F4F3EE] rounded-3xl'>
+              <img className='bg-transparent m-auto' src={img} alt='monkey'/>
       </div>
-      <div>
+      <div className='sm:w-2/3 sm:mx-32 my-16'>
+          <div className='text-[#FFFF00] font-gilroy my-7'>
+                ABOUT US ---
+          </div>
+          <div className='my-7 font-gilroy text-[#FFFFFF] text-3xl lg:text-5xl font-bold ' >
+              Your Brand, Our Reach, <br/> a saga forged in heaven
+          </div>
 
+        <div className='hidden sm:block '>
+            <div className='font-gilroy' >
+              <div className='my-5 mx-0 text-xl text-justify'>
+                Welcome to the jungle of Mad Monkey. To put into layman’s terms, we are a group of service providers who “curate-with-care”, the most impactful solutions to your marketing needs. 
+              </div>
+              <div className='my-5 mx-0 text-xl text-justify'>
+              In other words, we put the “fun” in the “fun-damentals” of your marketing needs, from transformation, development to execution. 
+              </div>
+              <div className='my-5 mx-0 text-xl text-justify'>
+              Our motto is to customize solutions to suit your Brand needs that make an impact in the market!
+              </div>
+              <div className='my-5 mx-0 text-xl text-justify'>
+              Connect with us to leave your worry behind and go ballistic into the wide world of brands, and this is our Madmonkey promise.
+              </div>
+            </div>
+        </div>
       </div>
     </div>
   )
--- mad_monkey/src/components/Navbar.js
+++ mad_monkey/src/components/Navbar.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link} from 'react-router-dom'
+import Hamburger from 'hamburger-react'
 
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
   return (
-    <div className='flex'>
+    <div className='flex mt-[40px]'>
         <Link className='p-4 font-junkdog text-[#FF6E1A] text-[36px]' to='/'>Mad Monkey</Link>
-        <Link className='p-4' to='/contact'>Contact</Link>
-        <Link className='p-4' to='/casestudies'>CaseStudies</Link>
+
+        <div className= {`block lg:hidden m-auto`} onClick= { () => setOpen(!open)} >
+          <Hamburger />
+        </div>
+
+        <div className='hidden lg:block absolute left-1/4 mt-4  p-4 m-auto'>
+          <Link className='' to='/contact'>Contact</Link>
+          <Link className='' to='/casestudies'>CaseStudies</Link>
+
+        </div>
+        <div className={`${open ? 'flex' : 'hidden'} mt-32 absolute`}>
+                <ul className='flex'>
+                  <li><Link className='' to='/contact'>Contact</Link></li>
+                  <li><Link className='' to='/casestudies'>CaseStudies</Link></li>
+                  {/* <li>Content</li>
+                  <li>Content</li> */}
+
+                </ul>
+          </div>
     </div>
   )
 }
