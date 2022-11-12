2602c6b35e4128f2987a95a9fe7a329a4e470b4f
Giriraj-Roy <girirajroy2001@gmail.com>
2022-11-12 14:11:34 +0530

    Footer in progress

--- mad_monkey/src/components/Footer.js
+++ mad_monkey/src/components/Footer.js
@@ -3,14 +3,54 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <div>
-        
-      </div>
-      <div className='w-full h-[50px] flex justify-center bg-[#28D596] ' >
-          <div className='mt-2 bg-transparent  text-black text-2xl font-gilroy font-semibold '>
-            Designed & built with passion in India 🚀
+      <div className='bg-[#FFFFFF] flex w-full aspect-[3.5/1] text-black'>
+        <div className='m-auto block bg-transparent'>
+          <span className=' bg-transparent font-junkdog text-5xl text-[#FF6E1A]'>MAD MONKEY</span>
+          <br/>
+          <span className='bg-transparent font-gilroy font-semibold text-2xl text-[#E63946] '>India’s Leading Product Marketing Agency</span>
+        </div>
+        <div className='m-auto block bg-transparent'>
+          <span className='bg-transparent font-gilroy font-extrabold text-2xl text-[#E63946] '>Navigation</span>
+          <br/><br/>
+          <div className='font-gilroy bg-transparent'>
+            <ul className='bg-transparent '>
+              <li className='bg-transparent text-[#0D0D0D] text-xl font-semibold'>Home</li>
+              <br/>
+              <li className='bg-transparent text-[#0D0D0D] text-xl font-semibold'>About Us</li>
+              <br/>
+              <li className='bg-transparent text-[#0D0D0D] text-xl font-semibold'>Case Studies</li>
+              <br/>
+              <li className='bg-transparent text-[#0D0D0D] text-xl font-semibold'>Contact Us</li>
+
+            </ul>
           </div>
+        </div>
+        <div className='m-auto bg-transparent'>
+          <span className='bg-transparent font-gilroy font-extrabold text-2xl text-[#E63946] '>Reach Us</span>
+          <br/>
+          <div className='font-gilroy bg-transparent'>
+            <ul className='bg-transparent '>
+              <li className='bg-transparent text-[#0D0D0D] text-xl font-semibold'>+91 123456789</li>
+              <br/>
+              <li className='bg-transparent text-[#0D0D0D] text-xl font-semibold'>hey@gmail.com</li>
+              <br/>
+            </ul>
+        </div>
+        <div className='m-auto bg-transparent'>
+          <span className='bg-transparent font-gilroy font-extrabold text-2xl text-[#E63946] '>Connect With Us</span>
+          <br/>
+          <span>Home</span>
+          <span>About Us</span>
+          <span>Case Studies</span>
+        </div>
       </div>
+      </div>
+        <div className='w-full h-[50px] flex justify-center bg-[#28D596] ' >
+            <div className='mt-2 bg-transparent  text-black text-2xl font-gilroy font-semibold '>
+              Designed & built with passion in India 🚀
+            </div>
+        </div>
+
     </>
   )
 }
