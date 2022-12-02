ff44fa881b79a0c1e741dd76320512b55461c622
Giriraj-Roy <girirajroy2001@gmail.com>
2022-12-02 17:34:26 +0530

    CaseStudies

--- mad_monkey/src/components/CaseStudies.js
+++ mad_monkey/src/components/CaseStudies.js
@@ -10,37 +10,38 @@ const CaseStudies = () => {
         <div className='font-gilroy font-bold text-5xl'>
             Some of our finest works
         </div>
-        <div className='w-full'>
-            <HorizontalScroll>
+        <div className='w-full aspect-[4/1]'>
+            {/* <HorizontalScroll> */}
             
-            <div className='w-[90%]'>
-                <div>
-                    Lorem
+            <div className='my-16 bg-[#FF5E19]  w-[95%] rounded-[3rem] aspect-[3/1] flex'>
+                <div className='mx-12 my-16 w-2/5 text-justify bg-transparent font-gilroy font-bold text-5xl '>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et et mattis nec cras viverra a cras suspendisse amet.
                 </div>
-                <div>
-                    kjc
-                </div>
-
-            </div>
-            <div className='w-[90%]'>
-                <div>
-                    Lorem
-                </div>
-                <div>
-
+                <div className='w-2/5 bg-white rounded-t-3xl mt-16 ml-10'>
+                    
                 </div>
 
             </div>
-            <div className='w-[90%]'>
-                <div>
-                    Lorem
+            <div className='my-16 bg-[#6A00FE]  w-[95%] rounded-[3rem] aspect-[3/1] flex'>
+                <div className='w-2/5 bg-white rounded-t-3xl mt-16 ml-12 '>
+
                 </div>
-                <div>
+                <div className='ml-16 my-16 w-2/5 text-justify bg-transparent font-gilroy font-bold text-5xl '>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et et mattis nec cras viverra a cras suspendisse amet.
+                </div>
+                
+
+            </div>
+            <div className='my-16 bg-[#07A157]  w-[95%] rounded-[3rem] aspect-[3/1] flex'>
+                <div className='mx-12 my-16 w-2/5 text-justify bg-transparent font-gilroy font-bold text-5xl '>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et et mattis nec cras viverra a cras suspendisse amet.
+                </div>
+                <div className='w-2/5 bg-white rounded-t-3xl mt-16 ml-10'>
 
                 </div>
 
             </div>
-            </HorizontalScroll>
+            {/* </HorizontalScroll> */}
         </div>
     </div>
   )
