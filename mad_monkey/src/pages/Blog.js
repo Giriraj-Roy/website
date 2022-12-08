a9e02bfabacb20b8adcd75a065a83e2b3e2d538e
Giriraj-Roy <girirajroy2001@gmail.com>
2022-12-08 16:16:11 +0530

    Blog Page

--- mad_monkey/src/pages/Blog.js
+++ mad_monkey/src/pages/Blog.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import HotTopic from '../components/BlogPage/HotTopic'
+import Trending from '../components/BlogPage/Trending'
+import Latest from '../components/BlogPage/Latest'
+
+
 
 const Blog = () => {
   return (
     <div className='text-orange-500'>
-        This is Blog
+        <HotTopic/>
+        <Trending/>
+        <Latest/>
     </div>
   )
 }
